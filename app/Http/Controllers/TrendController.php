0c5a9b726fcc90ca803d3be10cd321e69a62718a
8server <mizo12201220@gamil.com>
2023-06-09 08:22:44 +0900

    Google Trends APIをインストールしました

--- app/Http/Controllers/TrendController.php
+++ app/Http/Controllers/TrendController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Trend;
+use XFran\GTrends\GTrends;
 
 class TrendController extends Controller
 {
-    public function index(Trend $trend)
-  {
-    return view('trends/index')->with(['trends' => $trend ->get()]);
+    public function index()
+{
+    
+    $options = [
+    'hl' => 'ja-JP',
+    'tz' => -540,
+    'geo' => 'JP',
+    ];
+    $gt = new GTrends($options);
+    
+    $keyWordList = ['Java'];
+    $category = 0;
+    $time = 'today 12-m';
+    $property = '';
+    
+    $result = $gt->explore($keyWordList, $category, $time, $property);
+        
+    dd($result);
+        
+    return view('trends/index')->with(['trends' => $result]);
   }
 }
--- resources/views/trends/index.blade.php
+++ resources/views/trends/index.blade.php
@@ -7,16 +7,20 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         
+        <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Index') }}
+        </h2>
+         </x-slot>
+        
     </head>
     <body class="antialiased">
        <h1>Trend Name</h1>
        <div class = 'trends'>
-           @foreach ($trends as $trend)
-           <div class = 'trend'>
-               <h2 class = 'title'>{{$trend->title}}</h2>
-               <p class = 'body'>{{$trend->body}}</p>
-           </div>
-           @endforeach
        </div>
     </body>
+    
+    </x-app-layout>
+    
 </html>
